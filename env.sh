88cfcc07f24c0aad67325c1e1bb3c5ec834cd17e
Atle Frenvik Sveen <atle@frenviksveen.net>
2016-03-17 23:10:20 +0100

    ignore

--- env.sh
+++
@@ -1,4 +0,0 @@
-export SECRET_KEY=TralalaSamMannenOgææ09
-export DEBUG=True
-export SQLALCHEMY_DATABASE_URI=postgresql://beer:beer@localhost:5432/beer
-export WEBATLAS_API_TOKEN=08b24ea2-8b25-443d-b700-652432d16d83
